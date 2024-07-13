26e169c66b62284b4312a2f568ae48ce4986771c
Gunjan <gunjanmadaan06@gmail.com>
2024-07-13 12:35:21 +0530

    first commit

---
+++ Day1/Datatypes.java
@@ -0,0 +1,13 @@
+package Day1;
+import java.util.Scanner;
+public class Datatypes{
+    public static void main(String[] args){
+        //Input
+        Scanner sc = new Scanner(System.in);//take input line from the system.
+        int a =sc.nextInt();
+        //output
+        System.out.print("Value input by user: ");
+        System.out.println(a);
+        sc.close();
+    }
+}
